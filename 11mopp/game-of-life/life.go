3678aa33d68df567e439aabd444434a33b4efcca
NiloofarGheibi <niloofar.gheibi@gmail.com>
2017-06-11 11:48:27 +0200

    [Game of Life][Go] Book Approach

--- 11mopp/game-of-life/life.go
+++ 11mopp/game-of-life/life.go
@@ -1,113 +1,227 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"runtime"
 	"strconv"
 	"strings"
-	"sync"
 	"unicode"
 )
 
 var fin *string
-
-type message struct {
-	count  []int
-	row    []int
-	row_id int
-}
-
-type result struct {
-	row    []int
-	row_id int
-}
 
 func must(err error) {
 	if err != nil {
 		log.Print(err)
 	}
 }
-func allocate_board(size int) [][]int {
-	board := make([][]int, size)
+func allocate_board(size int) []uint8 {
+	board := make([]uint8, size*size)
 	for i := range board {
-		board[i] = make([]int, size)
+		board[i] = 0
 	}
 	return board
 }
 
-/* return the number of on cells adjacent to the i,j cell */
-func adjacent_to(board [][]int, size int, i int, j int) int {
-	var sk, ek, sl, el int
-	k := 0
-	l := 0
-	count := 0
+func _Cell_Turn_On(board *[]uint8, row int, col int, size int) {
+	var xoleft, xoright, yoabove, yobelow int
 
-	if i > 0 {
-		sk = i - 1
+	// OFFSET CALCULATING
+
+	if row == 0 {
+		xoleft = size - 1
 	} else {
-		sk = i
+		xoleft = -1
 	}
 
-	if i+1 < size {
-		ek = i + 1
+	if col == 0 {
+		yoabove = size * (size - 1)
 	} else {
-		ek = i
+		yoabove = -size
 	}
 
-	if j > 0 {
-		sl = j - 1
+	if row == size-1 {
+		xoright = -(size - 1)
 	} else {
-		sl = j
+		xoright = 1
 	}
-
-	if j+1 < size {
-		el = j + 1
+	if col == size-1 {
+		yobelow = -size * (size - 1)
 	} else {
-		el = j
+		yobelow = size
 	}
+	place := col*size + row
+	(*board)[place] |= 0x01 // first bit = state
 
-	for k = sk; k <= ek; k++ {
+	(*board)[place+yoabove+xoleft] += 2
+	(*board)[place+yoabove] += 2
+	(*board)[place+yoabove+xoright] += 2
+	(*board)[place+xoleft] += 2
+	(*board)[place+xoright] += 2
+	(*board)[place+yobelow+xoleft] += 2
+	(*board)[place+yobelow] += 2
+	(*board)[place+yobelow+xoright] += 2
 
-		for l = sl; l <= el; l++ {
-			count += board[k][l]
-			//fmt.Printf(" value of count %v \n", count)
-		}
-	}
-
-	count = count - board[i][j]
-
-	return count
 }
 
-func play(board [][]int, size int, a [][]int) [][]int {
+func _Cell_Turn_Off(board *[]uint8, row int, col int, size int) {
+	var xoleft, xoright, yoabove, yobelow int
+
+	// OFFSET CALCULATING
+
+	if row == 0 {
+		xoleft = size - 1
+	} else {
+		xoleft = -1
+	}
+
+	if col == 0 {
+		yoabove = size * (size - 1)
+	} else {
+		yoabove = -size
+	}
+
+	if row == size-1 {
+		xoright = -(size - 1)
+	} else {
+		xoright = 1
+	}
+	if col == size-1 {
+		yobelow = -size * (size - 1)
+	} else {
+		yobelow = size
+	}
+
+	place := col*size + row
+
+	(*board)[place] &= 0xFE // first bit = state
+
+	(*board)[place+yoabove+xoleft] -= 2
+	(*board)[place+yoabove] -= 2
+	(*board)[place+yoabove+xoright] -= 2
+	(*board)[place+xoleft] -= 2
+	(*board)[place+xoright] -= 2
+	(*board)[place+yobelow+xoleft] -= 2
+	(*board)[place+yobelow] -= 2
+	(*board)[place+yobelow+xoright] -= 2
+}
+
+func Next_Generation(board []uint8, size int) []uint8 {
+
+	for y := 0; y < size; y++ {
+		x := 0
+		//fmt.Println("before do place = ", place)
+		for x < size {
+
+			// Off and no neighbors
+			for board[(y*size)+x] == 0 {
+				fmt.Println("pass")
+				x++
+				if x >= size {
+					goto RowDone
+				}
+
+			}
+			// 0 0 0 0   0 0 0 0
+			a := (board[(y*size)+x] >> 1) & 0x17 // Number of neighbors
+			fmt.Println("Neigbours = ", a, "Value = ", board[(y*size)+x]&0x01)
+
+			if board[(y*size)+x]&0x01 == 1 {
+				if a != 2 && a != 3 {
+					_Cell_Turn_Off(&board, y, x, size)
+				}
+
+			} else {
+				if a == 3 {
+					_Cell_Turn_On(&board, y, x, size)
+				}
+			}
+			x++
+		}
+	RowDone:
+	}
+	return board
+}
+
+func play(board []uint8, size int) []uint8 {
 	//var a int
 	newboard := allocate_board(size)
 	/* for each cell, apply the rules of Life */
-	for i := 0; i < size; i++ {
-		for j := 0; j < size; j++ {
+	for y := 0; y < size; y++ {
+		for x := 0; x < size; x++ {
 
-			if board[i][j] == 0 && a[i][j] == 0 {
-				continue
-			} else {
+			// if board[(y*size)+x] == 0 {
+			// 	continue
+			// } else {
 
-				if a[i][j] == 2 {
-					newboard[i][j] = board[i][j]
-				}
-
-				if a[i][j] == 3 {
-					newboard[i][j] = 1
-				}
-				if a[i][j] < 2 {
-					newboard[i][j] = 0
-				}
-				if a[i][j] > 3 {
-					newboard[i][j] = 0
+			for board[(y*size)+x] == 0 {
+				x++
+				if x >= size {
+					goto RowDone
 				}
 			}
+			a := (board[(y*size)+x] >> 1) & 0x17 // Number of neighbors
+			if a == 2 {
+				newboard[(y*size)+x] = board[(y*size)+x] & 0x01
+			}
+
+			if a == 3 {
+				newboard[(y*size)+x] = 1
+
+			}
+			if a < 2 {
+				newboard[(y*size)+x] = 0
+			}
+			if a > 3 {
+				newboard[(y*size)+x] = 0
+			}
+
+		}
+	RowDone:
+	}
+
+	// Counting the neighbors
+	for y := 0; y < size; y++ {
+		for x := 0; x < size; x++ {
+			count := uint8(0)
+			var xoleft, xoright, yoabove, yobelow int
+			if y == 0 {
+				xoleft = size - 1
+			} else {
+				xoleft = -1
+			}
+
+			if x == 0 {
+				yoabove = size * (size - 1)
+			} else {
+				yoabove = -size
+			}
+
+			if y == size-1 {
+				xoright = -(size - 1)
+			} else {
+				xoright = 1
+			}
+			if x == size-1 {
+				yobelow = -size * (size - 1)
+			} else {
+				yobelow = size
+			}
+			place := x*size + y
+
+			count += newboard[place+yoabove+xoleft] & 0x01
+			count += newboard[place+yoabove] & 0x01
+			count += newboard[place+yoabove+xoright] & 0x01
+			count += newboard[place+xoleft] & 0x01
+			count += newboard[place+xoright] & 0x01
+			count += newboard[place+yobelow+xoleft] & 0x01
+			count += newboard[place+yobelow] & 0x01
+			count += newboard[place+yobelow+xoright] & 0x01
+
+			newboard[place] |= count << 1
+
 		}
 	}
 
@@ -116,12 +230,12 @@ func play(board [][]int, size int, a [][]int) [][]int {
 }
 
 /* print the life board */
-func print(board [][]int, size int) {
+func print(board []uint8, size int) {
 	/* for each row */
 	for j := 0; j < size; j++ {
 		/* print each column position... */
 		for i := 0; i < size; i++ {
-			if board[i][j] == 1 {
+			if board[(j*size)+i]&0x01 == 1 {
 				fmt.Printf("%c", 'x')
 			} else {
 				fmt.Printf("%c", ' ')
@@ -141,142 +255,92 @@ func SpaceMap(str string) string {
 	}, str)
 }
 
-func Get(file []byte) (int, int, [][]int) {
+func Get(file []byte) (int, int, []uint8) {
 
 	str := string(file) // convert content to a 'string'
 	split := strings.SplitAfter(str, "\n")
-
-	// string to parse
-	//log.Println("1 line : ", split[0])
 	ints := strings.SplitAfter(split[0], " ")
-	//log.Println("first line data: ", ints[0], ints[1])
 	size, err := strconv.Atoi(SpaceMap(ints[0]))
 	must(err)
 	steps, err := strconv.Atoi(SpaceMap(strings.SplitAfter(ints[1], "\n")[0]))
 	must(err)
-	//v := "10"
 	if s, err := strconv.Atoi(string(ints[0])); err == nil {
 		fmt.Printf("%T, %v\n", s, s)
 	}
-	//log.Println("size: , steps: ", size, steps)
 	board := allocate_board(size)
-	for j := 1; j <= size; j++ {
-		/* get a string */
-		/* copy the string to the life board */
-		//fmt.Println("j = ", j, "=> ", split[j])
+
+	for j := 1; j < size; j++ {
 		for i := 0; i < size; i++ {
 			if byte(split[j][i]) == 'x' {
-				board[i][j-1] = 1
-				//log.Println("x")
+				board[(j-1)*size+i] = 1
 			} else {
-				board[i][j-1] = 0
-				//log.Println("_")
+				board[(j-1)*size+i] = 0
+			}
+		}
+	}
+
+	// Counting the neighbors
+	for y := 0; y < size; y++ {
+		for x := 0; x < size; x++ {
+			count := uint8(0)
+			var xoleft, xoright, yoabove, yobelow int
+			if y == 0 {
+				xoleft = size - 1
+			} else {
+				xoleft = -1
 			}
 
+			if x == 0 {
+				yoabove = size * (size - 1)
+			} else {
+				yoabove = -size
+			}
+
+			if y == size-1 {
+				xoright = -(size - 1)
+			} else {
+				xoright = 1
+			}
+			if x == size-1 {
+				yobelow = -size * (size - 1)
+			} else {
+				yobelow = size
+			}
+			place := x*size + y
+
+			count += board[place+yoabove+xoleft] & 0x01
+			count += board[place+yoabove] & 0x01
+			count += board[place+yoabove+xoright] & 0x01
+			count += board[place+xoleft] & 0x01
+			count += board[place+xoright] & 0x01
+			count += board[place+yobelow+xoleft] & 0x01
+			count += board[place+yobelow] & 0x01
+			count += board[place+yobelow+xoright] & 0x01
+
+			board[place] |= count << 1
+
 		}
-		//fscanf(f,"\n");
 	}
 
 	return size, steps, board
 }
 
-func init() {
-
-	fin = flag.String("in", "judge.in", "input file")
-	flag.Parse()
-
-}
-func Count(board [][]int, size int) [][]int {
-	c := allocate_board(size)
-	// updating the count
-	for j := 0; j < size; j++ {
-		for i := 0; i < size; i++ {
-			c[j][i] = adjacent_to(board, size, j, i)
-		}
-		//fmt.Println("count = ", c[j])
-	}
-	return c
-}
-
-func worker(tasksCh <-chan message, wg *sync.WaitGroup, size int, next chan result) {
-	defer wg.Done()
-	for {
-		newboard := make([]int, size)
-		task, ok := <-tasksCh
-		if !ok {
-			return
-		}
-		//fmt.Println("processing task", task.row_id, "= ", task.row)
-		//fmt.Println(task.row_id, " ", "count", task.count)
-		// ____________ Processing ____________
-		for j := 0; j < size; j++ {
-
-			if task.row[j] == 0 && task.count[j] == 0 {
-				continue
-			} else {
-				if task.count[j] == 2 {
-					newboard[j] = task.row[j]
-				}
-
-				if task.count[j] == 3 {
-					newboard[j] = 1
-				}
-				if task.count[j] < 2 {
-					newboard[j] = 0
-				}
-				if task.count[j] > 3 {
-					newboard[j] = 0
-				}
-			}
-		}
-		//_______________ END ________________
-		next <- result{newboard, task.row_id}
-	}
-}
-
-func pool(wg *sync.WaitGroup, workers, tasks int, board [][]int, a [][]int, next chan result) {
-	tasksCh := make(chan message)
-
-	for i := 0; i < workers; i++ {
-		go worker(tasksCh, wg, tasks, next)
-	}
-
-	for i := 0; i < tasks; i++ {
-		tasksCh <- message{a[i], board[i], i}
-	}
-
-	close(tasksCh)
-
-}
-
 func main() {
+
 	file, err := ioutil.ReadAll(os.Stdin)
 	must(err)
 	size, steps, prev := Get(file)
-	var tmp, n [][]int
-	for i := 0; i < steps; i++ {
-		var wg sync.WaitGroup
-		runtime.GOMAXPROCS(runtime.NumCPU())
-		workers := runtime.NumCPU()
-		next := make(chan result, size)
-		c := Count(prev, size)
-		wg.Add(workers)
-		go pool(&wg, workers, size, prev, c, next)
-		wg.Wait()
-		close(next)
-		// Data for each row is ready
-		//fmt.Println("After WAIT")
-		n = allocate_board(size)
-		for i := range next {
-			//fmt.Println("Row = ", i.row, "id = ", i.row_id)
-			n[i.row_id] = i.row
-		}
 
+	var tmp []uint8
+	for i := 0; i < steps; i++ {
+		//n := Next_Generation(prev, size)
+		n := play(prev, size)
+		//fmt.Println(i, "____________")
+		//print(n, size)
 		//print(n, size)
 		tmp = n
 		n = prev
 		prev = tmp
 	}
 	print(prev, size)
-
 }
